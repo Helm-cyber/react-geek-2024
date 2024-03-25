92f3d11267899f4d1a624610ca403ead99915453
hlm <hlm@hlm.com>
2024-03-25 22:12:54 +0800

    封装token的工具方法

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,16 +2,19 @@
 
 import { request } from "@/utils"
 import { createSlice } from "@reduxjs/toolkit"
+import { setToken as _setToken, getToken, removeToken } from '@/utils'
 
 const userStore = createSlice({
   name: 'user',
   initialState: { // 初始化状态变量
-    token: localStorage.getItem('token_key') || '' // 能取到就用本地的，取不到就用空串，重新获取token
+    // token: localStorage.getItem('token_key') || '' // 能取到就用本地的，取不到就用空串，重新获取token
+    token: getToken() || '' // 优化后的代码
   },
   reducers: { // 设置同步的修改方法
     setToken (state, action) {
       state.token = action.payload
-      localStorage.setItem('token_key', action.payload) // localStorage存一份
+      // localStorage.setItem('token_key', action.payload) // localStorage存一份
+      _setToken(action.payload) // 优化后的代码
     }
   }
 })
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 // 统一中转导出工具模块函数，以后只需import { ... } from '@/utils'
 import { request } from "./request"
+import { setToken, getToken, removeToken } from './token'
 
-export { request }
+export { request, setToken, getToken, removeToken }
