aecdc90c05b67021adf1eb57c1daf03f33b84e26
hlm <hlm@hlm.com>
2024-03-25 20:29:50 +0800

    登录功能-表单校验和数据收集

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,46 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
 const Login = () => {
+  const onFinish = (values) => {
+    console.log(values)
+  }
+
   return (
-    <div>This is Login Page</div>
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        {/* validateTrigger属性：统一设置字段触发验证的时机，这里统一设置为失焦时触发验证 */}
+        {/* onFinish回调函数：通过回调函数的参数，获取用户输入的内容 */}
+        <Form validateTrigger="onBlur" onFinish={onFinish}>
+          <Form.Item
+            name="mobile"
+            // 多条校验逻辑时，从上到下依次校验，当上面的不通过时，就不会往后校验
+            rules={[
+              { required: true, message: '请输入手机号！' },
+              { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机号格式！' }
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              { required: true, message: '请输入验证码！' }
+            ]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   )
 }
 
